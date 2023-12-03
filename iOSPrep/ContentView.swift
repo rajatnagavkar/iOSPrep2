bd64bde4a2ed26018ca37cf8a969621caec6d61c
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2023-12-03 11:49:42 -0500

    Created Xcode Project

---
+++ iOSPrep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSPrep
+//
+//  Created by Rajat Nagavkar on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
